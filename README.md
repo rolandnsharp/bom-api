e860de7894d5b87664db4af7a8c9900f0d429993
Roland Sharp <rolandnsharp@gmail.com>
2014-05-22 13:16:15 +1000

    readme example functions and returned values

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
-#bom-api
+#bom-api#
 
 node wrapper for BOM (bereau of meteorology) api
+
+#Use#
+
+    npm install bom-api 
+
+    var bom = require('bom-api');
+
+Example:
+
+    bom.getBomDataBySiteNumberState(stateName, siteNumber, function(error, bomInfoArray)){
+        // do stuff with bomInfoArray
+    });
+
+    var bomInfoArray = bom.getBomDataByLatitudeLongitude(lat, lng);
+
+
+Example of the returned data from bomInfoArray.
+
+    [{
+    air_temp: 24.5,
+    apparent_t: 25.8,
+    rel_hum: 60,
+    local_date_time_full: "20140522120000",
+    utc_date_time_full: "20140522020000"
+    },
+    {
+    air_temp: 24.9,
+    apparent_t: 26.5,
+    rel_hum: 61,
+    local_date_time_full: "20140522113000",
+    utc_date_time_full: "20140522013000"
+    },
+    {
+    air_temp: 23.1,
+    apparent_t: 24.1,
+    rel_hum: 66,
+    local_date_time_full: "20140522110000",
+    utc_date_time_full: "20140522010000"
+    }........
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 var bomdata = require('./bomdata.json');
-var f = require('./functions');
+var helperFunctions = require('./functions');
 
 var IDCodes = {
     tas: 'IDT60801',
@@ -13,11 +13,10 @@ var IDCodes = {
     act: 'IDN60903'
 };
 
-function getBomInfo(stateName, siteNumber, callback) {
-
+function getBomDataBySiteNumberState(stateName, siteNumber, callback) {
     console.log(stateName, siteNumber);
     var url = 'http://www.bom.gov.au/fwo/' + IDCodes[stateName] + '/' + IDCodes[stateName] + '.' + siteNumber + '.json';
-
+    
     request(url, function(error, response, body) {
         try {
             var data = JSON.parse(body);
@@ -28,33 +27,34 @@ function getBomInfo(stateName, siteNumber, callback) {
 
         var observationData = data.observations.data;
         var bomInfoArray = [];
+        
         for (var i = 0; i < observationData.length; i++) {
 
             var bomInfo = {
                 air_temp: observationData[i].air_temp,
                 apparent_t: observationData[i].apparent_t,
                 rel_hum: observationData[i].rel_hum,
-                local_date_time_full: observationData[i].local_date_time_full
+                local_date_time_full: observationData[i].local_date_time_full,
+                utc_date_time_full: observationData[i].aifstime_utc
             };
             bomInfoArray.push(bomInfo);
         };
         callback(null, bomInfoArray);
-
     });
 };
 
-function getClosestWeatherData(lat, lng) {
+function getBomDataByLatitudeLongitude(lat, lng) {
     var numStates = Object.keys(bomdata).length;
 
-    var done = f.createWhenDoneCallback(numStates, function(locations) {
-        var closest = f.getClosestLocation(locations);
-        getBomInfo(closest.state, closest.location.siteNumber, function(error, bomInfo) {
+    var done = helperFunctions.createWhenDoneCallback(numStates, function(locations) {
+        var closest = helperFunctions.getClosestLocation(locations);
+        getBomDataBySiteNumberState(closest.state, closest.location.siteNumber, function(error, bomInfo) {
             console.log(JSON.stringify(bomInfo));
             return JSON.stringify(bomInfo);
         });
     });
     for (var stateName in bomdata) {
-        f.getLocationDistancesByState(
+        helperFunctions.getLocationDistancesByState(
             lat,
             lng,
             stateName,
@@ -62,7 +62,8 @@ function getClosestWeatherData(lat, lng) {
         );
     }
 }
+
 module.exports = {
-    getBomInfo: getBomInfo,
-    getClosestWeatherData: getClosestWeatherData
+    getBomDataBySiteNumberState: getBomDataBySiteNumberState,
+    getBomDataByLatitudeLongitude: getBomDataByLatitudeLongitude
 };
